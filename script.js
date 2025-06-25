abf530ae254ff7bc74b1cf69462041ee384672ae
nour roubhi <145440687+nour-rb@users.noreply.github.com>
2025-06-25 19:04:39 +0100

    fix: improve prompt logic

--- script.js
+++ script.js
@@ -17,8 +17,14 @@ squares.forEach(square => {
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-  const input = prompt("Enter grid size (maximum: 100)")
-  if (input === null){
-    alert("Please enter a number between 1 and 100.")
-  }
+  let input;
+  do {
+    input = prompt("Enter grid size (maximum: 100)");
+    if (input === null){
+    alert("You cancelled. Please enter a number between 1 and 100.")
+  } else if (isNaN(input) || input > 100 || input < 1){
+    alert("Invalid input. Please enter a number between 1 and 100.");
+  }} while (input === null || isNaN(input) || input < 1 || input > 100);
+
+  console.log(`You chose: ${input}`);
 })
